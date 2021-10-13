671704df373f8bcd557f070c03b1b0654b161f9e
Daniel Tellez <tellezfrancodanie@gmail.com>
2021-10-13 15:09:09 -0500

    v5

--- Hospitalizacion.App.FrontEnd/Pages/Signos/ListaSignos.cshtml.cs
+++ Hospitalizacion.App.FrontEnd/Pages/Signos/ListaSignos.cshtml.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Hospitalizacion.App.Dominio;
+using Hospitalizacion.App.Persistencia;
 
 namespace Hospimascotas.App.Frontend.Pages
 {
     public class ListaSignosModel : PageModel
     {
+
+        private readonly  IRepositorioSignosVitales _reportesigno;
+
+         public IEnumerable<SignosVitales> SignosVitalesl { get; set;}
+         
+
+         public ListaSignosModel(IRepositorioSignosVitales _reportesigno)
+         {
+             this._reportesigno=_reportesigno;
+         }
+                 
         public void OnGet()
         {
+            SignosVitalesl=_reportesigno.GetAllSignosVitales();
         }
     }
 }
